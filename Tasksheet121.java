c1b7c26bf14b5915c9647bdf33deb7591f219da5
misskrxx <chris.ann.carilla@gmail.com>
2024-10-25 16:22:22 +0800

    Tasksheet121.java

--- Tasksheet121.java
+++ Tasksheet121.java
@@ -15,10 +15,7 @@ public class Task121 {
     }
 
     public static int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed");
-        }
-        return floorDiv(a, b);
+        return Div(a, b);
     }
 
     public static void main(String[] args) {
